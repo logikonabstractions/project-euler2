466befab1ed3b4a0ce43aa416f569b51e9ab56ef
logicOnAbstractions <logiconabstractions@gmail.com>
2020-08-04 13:10:04 -0400

    problem 2 solved. an onsight.

--- main.js
+++ main.js
@@ -3,6 +3,12 @@
 * */
 
 import {compute_sum_ab_below} from './problems/problem_1.js';
+import {even_fibo} from "./problems/problem_2.js";
 
-// console.log(`and the answer is: ${compute_sum_ab_below(3, 5, 10)}`)
+// euler prob 1
 console.log(`and the answer is: ${compute_sum_ab_below()}`)
+
+// euler prob 1
+console.log(`and the answer is: ${even_fibo(1, 2, 100)}`)
+console.log(`and the answer is: ${even_fibo()}`)
+
